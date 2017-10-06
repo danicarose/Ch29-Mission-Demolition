3e0d7b031e9840406d0c41ef8da967e60d103dc6
danicarose <danica.calusin@gmail.com>
2017-10-06 15:59:17 -0400

    slingshot can fire projectile

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour {
 	//fields set in the Unity Inpspector pane
 	public GameObject	prefabProjectile;
+	public float 		velocityMult = 4f;
 	public bool 		_____________________________;
 
 	//fields set dynamically
@@ -12,8 +13,42 @@ public class Slingshot : MonoBehaviour {
 	public GameObject 	projectile;
 	public bool 		aimingMode;
 
+	void Update(){
+		//If Slingshot is not in aimingMode, don't run this code
+		if(!aimingMode) return;
+
+		//Get the current mouse position in 2D screen coordinates
+		Vector3 mousePos2D = Input.mousePosition;
+
+		//Convert the mouse position to 3D world coordinates
+		mousePos2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+
+		//Find the delta from the launchPos to the mousePos3D
+		Vector3 mouseDelta = mousePos3D-launchPos;
+
+		//Limit mouseDelta to the radius of the Slingshot SphereCollider
+		float maxMagnitude = this.GetComponent<SphereCollider>().radius; //THIS IS AN ERROR BUT WHY
+		if (mouseDelta.magnitude > maxMagnitude) {
+			mouseDelta.Normalize ();
+			mouseDelta *= maxMagnitude;
+		}
+
+		//Move the projectile to this new position
+		Vector3 projPos = launchPos + mouseDelta;
+		projectile.transform.position = projPos;
+
+		if (Input.GetMouseButtonUp (0)) {
+			//The mouse has been released
+			aimingMode = false;
+			projectile.GetComponent<Rigidbody>().isKinematic = false;
+			projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+			projectile = null;
+		}
+	}
+
 	void Awake(){
-		Transform launchPointTrans = transform.FindChild ("LaunchPoint");
+		Transform launchPointTrans = transform.Find ("LaunchPoint");
 		launchPoint = launchPointTrans.gameObject;
 		launchPoint.SetActive (false);
 		launchPos = launchPointTrans.position;
@@ -40,7 +75,7 @@ public class Slingshot : MonoBehaviour {
 		projectile.transform.position = launchPos;
 
 		//Set it to isKinematic for now
-		projectile.rigidbody.isKinematic = true;
+		projectile.GetComponent<Rigidbody>().isKinematic = true;
 	}
 
 }
